b15a1f24d56c7312408f1c3b857413f6b13ac010
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2026-01-17 05:24:37 +0000

    starting fresh

--- backend/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import authController from '../controllers/auth.controller.js';
-
-router.post('/register', authController.register);
-router.post('/Login', authController.Login);
-
-export default router;
